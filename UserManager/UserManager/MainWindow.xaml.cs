9898b4f69053791f52426c45d186b98d252c2535
BrutalSwede <pontus.bua@gmail.com>
2017-10-20 15:15:02 +0200

    Added backend code
    New features:
    *Add new user.
    *Elevate and demote users/admins
    *Buttons activate and deactivate depending on what is selected.

--- UserManager/UserManager/MainWindow.xaml.cs
+++ UserManager/UserManager/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,42 +26,103 @@ namespace UserManager
             InitializeComponent();
         }
 
-        private void UserListbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void Button_AddUser_Click(object sender, RoutedEventArgs e)
         {
-
+            // Input already validated at this point.
+            ListBox_Users.Items.Add(new User(TextBox_UserName.Text, TextBox_UserEmail.Text));
+            TextBox_UserName.Clear();
+            TextBox_UserEmail.Clear();
         }
 
-        private void AdminListbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void ListBox_Users_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            ListBox_Admins.SelectedIndex = -1;
+            if(ListBox_Users.SelectedIndex >= 0)
+            {
+                Button_ElevateUser.IsEnabled = true;
+                Button_EditUser.IsEnabled = true;
+            }
+            else
+            {
+                Button_ElevateUser.IsEnabled = false;
+                Button_EditUser.IsEnabled = false;
+            }
         }
 
-        private void ElevateUser_Click(object sender, RoutedEventArgs e)
+        private void ListBox_Admins_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            ListBox_Users.SelectedIndex = -1;
+            if(ListBox_Admins.SelectedIndex >= 0)
+            {
+                Button_DemoteAdmin.IsEnabled = true;
+                Button_EditUser.IsEnabled = true;
+            }
+            else
+            {
+                Button_DemoteAdmin.IsEnabled = false;
+                Button_EditUser.IsEnabled = false;
+            }
         }
 
-        private void DemoteAdmin_Click(object sender, RoutedEventArgs e)
+        private void Button_ElevateUser_Click(object sender, RoutedEventArgs e)
         {
-
+            ListBox_Admins.Items.Add(ListBox_Users.SelectedItem);
+            ListBox_Users.Items.Remove(ListBox_Users.SelectedItem);
         }
 
-        private void Add_User_Click(object sender, RoutedEventArgs e)
+        private void Button_DemoteAdmin_Click(object sender, RoutedEventArgs e)
         {
+            ListBox_Users.Items.Add(ListBox_Admins.SelectedItem);
+            ListBox_Admins.Items.Remove(ListBox_Admins.SelectedItem);
+        }
 
+        private void TextBox_UserName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (ValidateInputs(TextBox_UserName.Text, TextBox_UserEmail.Text))
+                Button_AddUser.IsEnabled = true;
+            else
+                Button_AddUser.IsEnabled = false;
+        }
+
+        private void TextBox_UserEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (ValidateInputs(TextBox_UserName.Text, TextBox_UserEmail.Text))
+                Button_AddUser.IsEnabled = true;
+            else
+                Button_AddUser.IsEnabled = false;
+        }
+
+        private bool ValidateInputs(string username, string email)
+        {
+            if (!string.IsNullOrWhiteSpace(username) && CheckIfEmail(email))
+                return true;
+            else
+                return false;
+        }
+
+        private bool CheckIfEmail(string input)
+        {
+            // We need to determine if what the user inputs is a valid email address.
+            string pattern = @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
+            if (Regex.IsMatch(input, pattern))
+                return true;
+            else
+                return false;
         }
     }
 
     public class User
     {
-        public string Name { get; private set; }
-        public string Email { get; private set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Privilege Privilege { get; set; }
 
 
         public User(string name, string email)
         {
             Name = name;
             Email = email;
+            Privilege = Privilege.User;
         }
 
         public string GetUserInfo()
@@ -68,7 +130,7 @@ namespace UserManager
 
             return ($"{Name} + {Email}");
         }
-
-
     }
+
+    public enum Privilege { User, Admin }
 }
